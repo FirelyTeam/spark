9c2f9a6364e4e6a6c19b653e645fbfde5906551c
Ewout Kramer <e.kramer@furore.com>
2014-02-27 16:48:08 +0100

    Added license info to each source code file

--- Spark/Configuration/MongoDbConnector.cs
+++ Spark/Configuration/MongoDbConnector.cs
@@ -1,4 +1,12 @@
-﻿using MongoDB.Bson.Serialization;
+﻿/* 
+ * Copyright (c) 2014, Furore (info@furore.com) and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.github.com/furore-fhir/spark/master/LICENSE
+ */
+
+using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Driver;
 using System;
