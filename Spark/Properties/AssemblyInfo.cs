3913fb1e47a75625aa413602f88b237087af6378
Ewout Kramer <e.kramer@furore.com>
2014-05-20 17:32:24 +0200

    Upgrade to post-DSTU version of .NET API

--- Spark/Properties/AssemblyInfo.cs
+++ Spark/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Spark2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("fURORE")]
+[assembly: AssemblyCompany("Furore")]
 [assembly: AssemblyProduct("Spark2")]
 [assembly: AssemblyCopyright("Copyright © Furore 2014")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
