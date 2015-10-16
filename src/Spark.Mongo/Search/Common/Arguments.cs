505c8bde3d6517909a6bcf19aac621863a66240b
Christiaan Knaap <c.knaap@furore.com>
2015-10-16 16:55:39 +0200

    Removed BuildQuery from Argument classes, since this was replaced by CriteriaMongoExtensions (some time ago already).

--- src/Spark.Mongo/Search/Common/Arguments.cs
+++ src/Spark.Mongo/Search/Common/Arguments.cs
@@ -41,11 +41,6 @@ namespace Spark.Mongo.Search.Common
         {
             return true;
         }
-        public virtual IMongoQuery BuildQuery(ITerm term)
-        {
-            string field = FieldToInternalField(term.Field);
-            return Query.EQ(field, term.Value);
-        }
         private static string FieldToInternalField(string field)
         {
             if (Config.Equal(field, UniversalField.ID)) field = InternalField.JUSTID;
@@ -59,10 +54,6 @@ namespace Spark.Mongo.Search.Common
         public MetaArgument(string field)
         {
             this.field = field;
-        }
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            return Query.EQ(term.Field, term.Value); 
         }
     }
    
@@ -78,31 +69,8 @@ namespace Spark.Mongo.Search.Common
             return "\"" + term.Value + "\"";
         }
 
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            string m = (term.Operator ?? string.Empty).ToLower();
-            switch (m)
-            {
-                case "exact": 
-                    return Query.EQ(term.Field, term.Value);
-                case "partial":
-                    return Query.Matches(term.Field, new BsonRegularExpression("^"+term.Value, "i")); //
-                case "phonetic":
-                    return Query.Matches(term.Field+"soundex", "^"+term.Value);
-                default:
-                    return Query.Matches(term.Field, new BsonRegularExpression(term.Value, "i")); //full ci
-            }
-        }
     }
-    public class TextArgument : Argument
-    {
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            IEnumerable<IMongoQuery> queries = term.Value.Split(' ').Select(s => Query.Matches(term.Field, new BsonRegularExpression(s, "i")));
-            return Query.And(queries);
-        }
-    }
-
+  
     public class IntArgument : Argument
     {
         public override string GroomElement(string value)
@@ -133,27 +101,6 @@ namespace Spark.Mongo.Search.Common
         {
             int i;
             return int.TryParse(value, out i);
-        }
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            switch(term.Operator)
-            {
-                case "<":
-                    return Query.LT(term.Field, term.Value);
-                case ">":
-                    return Query.GT(term.Field, term.Value);
-                case "<=":
-                    return Query.LTE(term.Field, term.Value);
-                case ">=":
-                    return Query.GTE(term.Field, term.Value);
-                case null:
-                case "=":
-                case "":
-                    return Query.EQ(term.Field, term.Value);
-                default:
-                    throw new SearchException(term.Operator+" is not a valid integer operator");
-
-            }
         }
     }
 
@@ -190,112 +137,10 @@ namespace Spark.Mongo.Search.Common
         }
         enum TokenStyle { Partial, Text, Code, AnyNs }
 
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            TokenStyle style;
-            string system, code;
-            string systemfield = term.Field+".system";
-            string codefield = term.Field+".code";
-            string displayfield = term.Field+".display";
-            string textfield = term.Field + "_text";
-
-            SplitToken(term.Value, out system, out code);
-            
-            if (string.IsNullOrEmpty(term.Operator))
-            {
-                style = TokenStyle.Partial;
-            }
-            else if (term.Operator == Modifier.TEXT)
-            {
-                style = TokenStyle.Text;
-            }
-            else if (term.Operator == Modifier.CODE)
-            {
-                style = TokenStyle.Code;
-            }
-            else if (term.Operator == Modifier.ANYNAMESPACE)
-            {
-                style = TokenStyle.AnyNs;
-            }
-            else throw new SearchException("Invalid Modifier: " + term.Operator);
-
-            switch(style)
-            {
-                case TokenStyle.Partial:
-                    return Query.Or(
-                        Query.Matches(codefield, new BsonRegularExpression(term.Value, "i")),
-                        Query.Matches(textfield, new BsonRegularExpression(term.Value, "i")),
-                        Query.Matches(displayfield, new BsonRegularExpression(term.Value, "i"))
-                    );  
-
-                case TokenStyle.Text:
-                    return Query.Or(
-                        Query.Matches(textfield, new BsonRegularExpression(term.Value, "i")),
-                        Query.Matches(displayfield, new BsonRegularExpression(term.Value, "i"))
-                    );
-
-                case TokenStyle.Code:
-                    if (system == null)
-                    {
-                        return Query.ElemMatch(term.Field, 
-                                Query.And(
-                                    Query.NotExists("system"),
-                                    Query.EQ("code", code)
-                                )
-                        );  
-
-                    }
-                    else
-                    {
-                        return Query.ElemMatch(term.Field,
-                            Query.And(
-                                Query.EQ("system", system),
-                                Query.EQ("code", code)
-                            )
-                        );  
-                    }
-
-                case TokenStyle.AnyNs:
-                    return Query.EQ(codefield, term.Value);
-
-                default:
-                    throw new SearchException("Impossible to get here.");
-            }
-        }
     }
 
     public class TagArgument : Argument
     {
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            IMongoQuery query;
-            
-            if (string.IsNullOrEmpty(term.Operator))
-            {
-                query = Query.EQ(InternalField.TAGTERM, term.Value);
-            }
-            else if (term.Operator == Modifier.PARTIAL)
-            {
-                query = Query.Matches(InternalField.TAGTERM, "^" + term.Value); // from the left
-            }
-            else if (term.Operator == Modifier.TEXT)
-            {
-                query = Query.EQ(InternalField.TAGLABEL, term.Value);
-
-            }
-            else
-            {
-                throw new SearchException("Invalid Modifier: " + term.Operator);
-            }
-
-            string scheme = "http://hl7.org/fhir/tag";
-            
-            return Query.ElemMatch(InternalField.TAG,
-                    Query.And(
-                        Query.EQ(InternalField.TAGSCHEME, scheme),
-                        query
-                    ));  
-        }
     }
 
     public class ReferenceArgument : Argument
@@ -321,16 +166,6 @@ namespace Spark.Mongo.Search.Common
         {
             term.Value = this.Groom(value);
         }
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            //TODO: Find a real solution for this, because I don't think it will work for
-            //a:Patient.b:Organization.c:Device=someid ?
-            if(!String.IsNullOrEmpty(term.Operator))
-                return Query.EQ(term.Field, term.Operator + "/" + term.Value);
-            else
-                return Query.Matches(term.Field, new BsonRegularExpression(".*" + term.Value));
-            //return Query.EQ(term.Field, term.Value);
-        }
 
     }
 
@@ -352,48 +187,6 @@ namespace Spark.Mongo.Search.Common
         {
             return Groom(value);
         }
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            string start = term.Field + ".start";
-            string end = term.Field + ".end";
-            string value = Groom(term.Value);
-            if (string.IsNullOrEmpty(term.Operator))
-            {
-                // omdat date parameter zowel voor datums als voor Periods wordt gebruikt, kan je helaas niets anders doen dan hier een OR van maken.
-
-                return 
-                    Query.Or(
-                        Query.Matches(term.Field, "^"+value),
-                        Query.And(
-                            Query.Or(Query.Exists(start), Query.Exists(end)),
-                            Query.Or(Query.LTE(start, value), Query.NotExists(start)),
-                            Query.Or(Query.GTE(end, value), Query.NotExists(end))
-                        )
-                    );
-            }
-            if (term.Operator == Modifier.AFTER)
-            {
-                return
-                    Query.Or(
-                        Query.GTE(term.Field, value),
-                        Query.GTE(start, value)
-                    );
-
-            }
-            else if (term.Operator == Modifier.BEFORE)
-            {
-                return
-                    Query.Or(
-                        Query.LTE(term.Field, value),
-                        Query.LTE(end, value)
-                    );
-            }
-            else
-            {
-                throw new SearchException(string.Format("The modifier [{0}] is invalid for date arguments", term.Operator));
-            }
-
-        }
     }
 
     public class FuzzyArgument : Argument
@@ -402,29 +195,6 @@ namespace Spark.Mongo.Search.Common
         {
             return Soundex.For(value);
         }
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            string value = Soundex.For(term.Value);
-            return Query.EQ(term.Field, value);
-        }
-    }
-
-    public class MissingArgument : Argument
-    {
-
-        public override IMongoQuery BuildQuery(ITerm term)
-        {
-            switch (term.Value.ToLower())
-            {
-                case "true":
-                    return Query.NotExists(term.Field);
-                case "false":
-                    return Query.Exists(term.Field);
-                default:
-                    throw new SearchException("Parameter ("+term.Field+") has a missing modifier and should have a value of 'true' or 'false'");
-            }
-        }
-
     }
 
     public static class ArgumentFactory
