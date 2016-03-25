0deceb8f5ca5f6aa98e245110869f5a2fa88b2a1
PaulDeac <Paul.Deac@isdc.eu>
2016-03-25 10:08:24 +0200

    bugfix: fixed CORS

--- src/Spark/App_Start/WebApiConfig.cs
+++ src/Spark/App_Start/WebApiConfig.cs
@@ -10,6 +10,7 @@ namespace Spark
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
