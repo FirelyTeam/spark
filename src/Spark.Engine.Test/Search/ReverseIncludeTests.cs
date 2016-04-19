4114f83db7e13ad97e70b89aba52e35fef15ee5a
Christiaan Knaap <c.knaap@furore.com>
2016-04-19 14:33:11 +0200

    _revinclude: Added structure for parsing and using ReverseInclude.

---
+++ src/Spark.Engine.Test/Search/ReverseIncludeTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spark.Engine.Search.Model;
+
+namespace Spark.Engine.Test.Search
+{
+    [TestClass]
+    public class ReverseIncludeTests
+    {
+        [TestMethod]
+        public void TestParseValid()
+        {
+            ReverseInclude sut = ReverseInclude.Parse("Patient.actor");
+
+            Assert.AreEqual("Patient", sut.ResourceType);
+            Assert.AreEqual("actor", sut.SearchPath);
+        }
+        [TestMethod]
+        public void TestParseValidLongerPath()
+        {
+            ReverseInclude sut = ReverseInclude.Parse("Provenance.target.patient");
+
+            Assert.AreEqual("Provenance", sut.ResourceType);
+            Assert.AreEqual("target.patient", sut.SearchPath);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestParseNull()
+        {
+            ReverseInclude sut = ReverseInclude.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestParseInvalid()
+        {
+            ReverseInclude sut = ReverseInclude.Parse("bla;foo");
+        }
+    }
+}
---
+++ src/Spark.Engine/Search/Model/ReverseInclude.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Spark.Engine.Search.Model
+{
+    public class ReverseInclude
+    {
+        private static Regex pattern = new Regex(@"(?<resourcetype>[^\.]+)\.(?<searchpath>.*)");
+
+        public string ResourceType { get; set; }
+        public string SearchPath { get; set; }
+
+        /// <summary>
+        /// Expected format: ResourceType.searchParameter[.searchParameter]*
+        /// </summary>
+        /// <param name="reverseInclude"></param>
+        /// <returns>ReverseInclude instance with ResourceType is everything before the first dot, and SearchPath everything after it.</returns>
+        public static ReverseInclude Parse(string reverseInclude)
+        {
+            //_revinclude should have the following format: ResourceType.searchParameter[.searchParameter]*
+            //so we simply split in on the first dot.
+            if (reverseInclude == null)
+            {
+                throw new ArgumentNullException("reverseInclude cannot be null");
+            }
+            var result = new ReverseInclude();
+            var match = pattern.Match(reverseInclude);
+            if (match.Groups.Count < 2)
+            {
+                throw new ArgumentException(String.Format("reverseInclude '{0}' does not adhere to the format 'ResourceType.searchParameter[.searchParameter]*'", reverseInclude));
+            }
+
+            result.ResourceType = match.Groups["resourcetype"].Captures[0].Value;
+            result.SearchPath = match.Groups["searchpath"].Captures[0].Value;
+
+            return result;
+        }
+    }
+}
