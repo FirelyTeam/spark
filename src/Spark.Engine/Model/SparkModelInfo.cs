3c46a8f3078dc9021da3ef012a655a2888b662f8
corina.ciocanea <corina.ciocanea@ISDC30598>
2016-09-05 20:54:33 +0300

    repair searching encounters by a serviceProvider chain criteria

--- src/Spark.Engine/Model/SparkModelInfo.cs
+++ src/Spark.Engine/Model/SparkModelInfo.cs
@@ -1,4 +1,5 @@
-﻿using Hl7.Fhir.Model;
+﻿using System;
+using Hl7.Fhir.Model;
 using System.Collections.Generic;
 using System.Linq;
 using static Hl7.Fhir.Model.ModelInfo;
@@ -10,9 +11,9 @@ namespace Spark.Engine.Model
         public static List<SearchParamDefinition> SparkSearchParameters = ModelInfo.SearchParameters.Union(
             new List<SearchParamDefinition>()
         {
-            new SearchParamDefinition() {Resource = "Composition", Name = "custodian", Description = @"custom search parameter on Composition for generating $document", Type = SearchParamType.Reference, Path = new string[] {"Composition.custodian" }, XPath = "f:Composition/f:custodian", Expression = "Composition.custodian" }
-            , new SearchParamDefinition() {Resource = "Composition", Name = "eventdetail", Description = @"custom search parameter on Composition for generating $document", Type = SearchParamType.Reference, Path = new string[] {"Composition.event.detail" }, XPath = "f:Composition/f:event/f:detail", Expression = "Composition.event.detail" }
-            , new SearchParamDefinition() {Resource = "Encounter", Name = "serviceprovider", Description = @"Organization that provides the Encounter services.", Type = SearchParamType.Reference, Path = new[] {"Encounter.serviceProvider"}, XPath = "f:Encounter/f:serviceProvider",Expression = "Encounter.serviceProvider" }
+            new SearchParamDefinition() {Resource = "Composition", Name = "custodian", Description = @"custom search parameter on Composition for generating $document", Type = SearchParamType.Reference, Path = new string[] {"Composition.custodian" }, XPath = "f:Composition/f:custodian", Expression = "Composition.custodian", Target =  new ResourceType[] { ResourceType.Organization}  }
+            , new SearchParamDefinition() {Resource = "Composition", Name = "eventdetail", Description = @"custom search parameter on Composition for generating $document", Type = SearchParamType.Reference, Path = new string[] {"Composition.event.detail" }, XPath = "f:Composition/f:event/f:detail", Expression = "Composition.event.detail", Target =  Enum.GetValues(typeof(ResourceType)).Cast<ResourceType>().ToArray()  }
+            , new SearchParamDefinition() {Resource = "Encounter", Name = "serviceprovider", Description = @"Organization that provides the Encounter services.", Type = SearchParamType.Reference, Path = new[] {"Encounter.serviceProvider"}, XPath = "f:Encounter/f:serviceProvider",Expression = "Encounter.serviceProvider", Target =  new ResourceType[] { ResourceType.Organization} }
             , new SearchParamDefinition() {Resource = "Slot", Name = "provider", Description = @"Search Slot by provider extension", Type = SearchParamType.Reference, Path = new string[] { @"Slot.extension(url=http://fhir.blackpear.com/era/Slot/provider).valueReference" } }
         }).ToList();
     }
