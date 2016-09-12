f87256c91439a9156b7a6fdd21dd76a7dc38528d
corina.ciocanea <corina.ciocanea@ISDC30598>
2016-09-12 11:17:30 +0300

    solve github bug #83

---
+++ src/Spark/Controllers/MaintenanceApiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using Spark.Core;
+using Spark.Engine.Interfaces;
+
+namespace Spark.Controllers
+{
+    [RoutePrefix("MaintenanceApi"), EnableCors("*", "*", "*", "*")]
+    public class MaintenanceApiController : ApiController
+    {
+        private IFhirStoreAdministration fhirStoreAdministration;
+        private IFhirIndex fhirIndex;
+
+        public MaintenanceApiController(IFhirStoreAdministration fhirStoreAdministration, IFhirIndex fhirIndex)
+        {
+            this.fhirStoreAdministration = fhirStoreAdministration;
+            this.fhirIndex = fhirIndex;
+        }
+        [HttpDelete, Route("ClearAll")]
+        public void ClearAll()
+        {
+            fhirStoreAdministration.Clean();
+            fhirIndex.Clean();
+        }
+    }
+}
