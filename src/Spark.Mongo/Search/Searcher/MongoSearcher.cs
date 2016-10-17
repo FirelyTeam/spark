9c0d70e984267f01524bf95aed76bd0fa7cb79de
corina.ciocanea <corina.ciocanea@ISDC30598>
2016-10-17 13:49:51 +0300

    implement sort order fot token, date and quantity parameters

--- src/Spark.Mongo/Search/Searcher/MongoSearcher.cs
+++ src/Spark.Mongo/Search/Searcher/MongoSearcher.cs
@@ -46,27 +46,13 @@ namespace Spark.Search.Mongo
             return new List<BsonValue>();
         }
 
-        private List<BsonValue> CollectSelfLinks(IMongoQuery query, IList<Tuple<string, SortOrder>> sortItems)
+        private List<BsonValue> CollectSelfLinks(IMongoQuery query, IMongoSortBy sortBy)
         {
             MongoCursor<BsonDocument> cursor = _collection.Find(query);
 
-
-            M.SortByBuilder builder = new M.SortByBuilder();
-            foreach (Tuple<string, SortOrder> sortItem in sortItems)
+            if (sortBy != null)
             {
-                if (sortItem.Item2 == SortOrder.Ascending)
-                {
-                    builder = builder.Ascending(sortItem.Item1);
-                }
-                else
-                {
-                    builder = builder.Descending(sortItem.Item1);
-                }
-            }
-
-            if (sortItems.Any())
-            {
-                cursor.SetSortOrder(builder);
+                cursor.SetSortOrder(sortBy);
             }
             cursor = cursor.SetFields(InternalField.SELFLINK);
 
@@ -107,14 +93,34 @@ namespace Spark.Search.Mongo
             return CollectKeys(resultQuery);
         }
 
-        private List<BsonValue> CollectSelfLinks(string resourceType, IEnumerable<Criterium> criteria, SearchResults results, int level, SearchParams searchCommand)
+        private List<BsonValue> CollectSelfLinks(string resourceType, IEnumerable<Criterium> criteria, SearchResults results, int level, IList<Tuple<string, SortOrder>> sortItems )
         {
             Dictionary<Criterium, Criterium> closedCriteria = CloseChainedCriteria(resourceType, criteria, results, level);
 
             //All chained criteria are 'closed' or 'rolled up' to something like subject IN (id1, id2, id3), so now we AND them with the rest of the criteria.
             IMongoQuery resultQuery = CreateMongoQuery(resourceType, results, level, closedCriteria);
+            IMongoSortBy sortBy = CreateSortBy(sortItems);
+            return CollectSelfLinks(resultQuery, sortBy);
+        }
 
-            return CollectSelfLinks(resultQuery, searchCommand.Sort);
+        private static IMongoSortBy CreateSortBy(IList<Tuple<string, SortOrder>> sortItems)
+        {
+            if (sortItems.Any() == false)
+                return null;
+
+            M.SortByBuilder builder = new M.SortByBuilder();
+            foreach (Tuple<string, SortOrder> sortItem in sortItems)
+            {
+                if (sortItem.Item2 == SortOrder.Ascending)
+                {
+                    builder = builder.Ascending(sortItem.Item1);
+                }
+                else
+                {
+                    builder = builder.Descending(sortItem.Item1);
+                }
+            }
+            return builder;
         }
 
         private static IMongoQuery CreateMongoQuery(string resourceType, SearchResults results, int level, Dictionary<Criterium, Criterium> closedCriteria)
@@ -347,10 +353,13 @@ namespace Spark.Search.Mongo
             {
                 results.UsedCriteria = criteria.Select(c => c.Clone()).ToList();
 
-                criteria = EnrichCriteriaWithSearchParameters(_fhirModel.GetResourceTypeForResourceName(resourceType), results);
+                criteria = EnrichCriteriaWithSearchParameters(_fhirModel.GetResourceTypeForResourceName(resourceType),
+                    results);
 
                 var normalizedCriteria = NormalizeNonChainedReferenceCriteria(criteria, resourceType);
-                List<BsonValue> selfLinks = CollectSelfLinks(resourceType, normalizedCriteria, results, 0, searchCommand);
+                var normalizeSortCriteria = NormalizeSortItems(resourceType, searchCommand);
+
+                List<BsonValue> selfLinks = CollectSelfLinks(resourceType, normalizedCriteria, results, 0, normalizeSortCriteria);
 
                 foreach (BsonValue selfLink in selfLinks)
                 {
@@ -360,6 +369,33 @@ namespace Spark.Search.Mongo
             }
 
             return results;
+        }
+
+        private IList<Tuple<string, SortOrder>> NormalizeSortItems(string resourceType, SearchParams searchCommand)
+        {
+            var sortItems = searchCommand.Sort.Select(s => NormalizeSortItem(resourceType, s)).ToList();
+            return sortItems;
+        }
+
+
+        private Tuple<string, SortOrder> NormalizeSortItem(string resourceType, Tuple<string, SortOrder> sortItem)
+        {
+            ModelInfo.SearchParamDefinition definition =
+                _fhirModel.FindSearchParameter(resourceType, sortItem.Item1)?.GetOriginalDefinition();
+
+            if (definition?.Type == SearchParamType.Token)
+            {
+                return new Tuple<string, SortOrder>(sortItem.Item1 + ".code", sortItem.Item2);
+            }
+            if (definition?.Type == SearchParamType.Date)
+            {
+                return new Tuple<string, SortOrder>(sortItem.Item1 + ".start", sortItem.Item2);
+            }
+            if (definition?.Type == SearchParamType.Quantity)
+            {
+                return new Tuple<string, SortOrder>(sortItem.Item1 + ".value", sortItem.Item2);
+            }
+            return sortItem;
         }
 
         public SearchResults GetReverseIncludes(IList<IKey> keys, IList<string> revIncludes)
