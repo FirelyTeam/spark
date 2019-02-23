0ecb08601a8538027c96afb11b88eeaa21257e6f
Kenneth Myhra <kennethmyhra@gmail.com>
2019-02-23 14:09:16 +0100

    Moved rest of javascript files
    * Moved from /Content/Scripts to /Scripts
    * This makes it easier when upgrading NuGet packages for these scripts

--- src/Spark/App_Start/BundleConfig.cs
+++ src/Spark/App_Start/BundleConfig.cs
@@ -14,11 +14,11 @@ namespace Spark
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Content/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Content/Scripts/respond.js"));
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                       "~/Content/bootstrap.css",
