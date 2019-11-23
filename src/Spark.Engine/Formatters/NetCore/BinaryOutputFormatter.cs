1fb5c08135dae27caf3cb4b1c0c8a8feb4a9694b
Kenneth Myhra <kennethmyhra@gmail.com>
2019-11-23 16:07:15 +0100

    Serve Binary resources via RESTful API
    * See: http://hl7.org/fhir/binary.html#rest
    * Tightening up access specifiers, setting various methods to
      private/internal that is only meant for internal code
    * Moving some code around for readability

--- src/Spark.Engine/Formatters/NetCore/BinaryOutputFormatter.cs
+++ src/Spark.Engine/Formatters/NetCore/BinaryOutputFormatter.cs
@@ -17,16 +17,23 @@ namespace Spark.Engine.Formatters
 
         protected override bool CanWriteType(Type type)
         {
-            return type == typeof(Binary) || type == typeof(FhirResponse);
+            return typeof(Binary).IsAssignableFrom(type) || typeof(FhirResponse).IsAssignableFrom(type);
         }
 
         public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
         {
-            if (typeof(Binary).IsAssignableFrom(context.ObjectType))
+            if (typeof(Binary).IsAssignableFrom(context.ObjectType) || typeof(FhirResponse).IsAssignableFrom(context.ObjectType))
             {
-                Binary binary = (Binary)context.Object;
-                var stream = new MemoryStream(binary.Content);
-                context.HttpContext.Response.Headers.Add("Content-Type", binary.ContentType);
+                Binary binary = null;
+                if (typeof(FhirResponse).IsAssignableFrom(context.ObjectType))
+                {
+                    FhirResponse response = (FhirResponse)context.Object;
+                    binary = response.Resource as Binary;
+                }
+                if (binary == null) return;
+
+                Stream stream = new MemoryStream(binary.Content);
+                context.HttpContext.Response.ContentType = binary.ContentType;
                 await stream.CopyToAsync(context.HttpContext.Response.Body);
             }
         }
