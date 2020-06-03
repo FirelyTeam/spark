c41c135d5b590b1381d20b97f913341a32da7c96
Jacob Young <a112554@amerisourcebergen.com>
2020-06-03 15:02:45 -0500

    fix key collistion error with lock pattern

--- src/Spark.Mongo/Search/Infrastructure/MongoDatabaseFactory.cs
+++ src/Spark.Mongo/Search/Infrastructure/MongoDatabaseFactory.cs
@@ -1,30 +1,29 @@
-﻿using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace Spark.Store.Mongo
 {
     public static class MongoDatabaseFactory
     {
-        private static Dictionary<string, IMongoDatabase> instances;
+        private static Dictionary<string, IMongoDatabase> _instances = new Dictionary<string, IMongoDatabase>();
+        private static readonly object _locker = new object();
 
         public static IMongoDatabase GetMongoDatabase(string url)
         {
-            IMongoDatabase result;
-        
-            if (instances == null) //instances dictionary is not at all initialized
+            if (_instances.Where(i => i.Key == url).Count() == 0) //there is no instance for this url yet
             {
-                instances = new Dictionary<string, IMongoDatabase>();
-            }
-            if (instances.Where(i => i.Key == url).Count() == 0) //there is no instance for this url yet
-            {
-                result = CreateMongoDatabase(url);
-                instances.Add(url, result);
-            }; 
-            return instances.First(i => i.Key == url).Value; //now there must be one.
+                lock (_locker)
+                {
+                    if (_instances.Where(i => i.Key == url).Count() == 0)
+                    {
+                        var result = CreateMongoDatabase(url);
+                        _instances.Add(url, result);
+                    }
+                }
+            };
+
+            return _instances.First(i => i.Key == url).Value; //now there must be one.
         }
 
         private static IMongoDatabase CreateMongoDatabase(string url)
