98d97dd0717414c0fbccc33324e2ef698d8a0f94
Andrew Anisimov <anisimov.new@gmail.com>
2020-12-09 21:10:10 +0300

    Write information notice with the summary results of test run.

--- tests/integration-tests/combine-test-results.sh
+++ tests/integration-tests/combine-test-results.sh
@@ -7,6 +7,9 @@ ANNOTATIONS_FILE=$2
 
 function usage() {
   me=`basename "$0"`
+  if [ ! -z "$1" ]; then
+    echo $1
+  fi
   echo "Usage: ./${me} path/to/input/json_results path/to/output/annotations.json"
   exit 1
 }
@@ -15,14 +18,24 @@ function usage() {
 
 [ ! -z "${JSON_PATH}" ] || usage
 
-[ -d "${JSON_PATH}" ] || usage
+[ -d "${JSON_PATH}" ] || usage "${JSON_PATH} must be a directory"
 
-touch $ANNOTATIONS_FILE || usage
+touch $ANNOTATIONS_FILE || "$ANNOTATIONS_FILE is not writable"
 
-ls ${JSON_PATH}/*.json | xargs jq -c '[ .[]
+DIR=$(pwd)
+
+cd ${JSON_PATH}
+
+# Have to use warning annotation level, notice isn't working anymore (but could be in future).
+
+SUMMARY=$(ls _summary*.json | xargs jq -c '[ .
+  | { "file": "summary",  "line": 1, "message": ("PASS: \(.pass // 0)\nFAIL: \(.fail // 0)\nERROR: \(.error // 0)\nSKIP: \(.skip // 0)"), "annotation_level": "warning" }
+]')
+
+FAILURES=$(ls -I '_summary*.json' | xargs jq -c '[ .[]
     | select(((.status == "skip") and (.message | contains("TODO") | not))
         or .status == "fail"
-	or .status == "error")
+        or .status == "error")
     | .id as $id
     | .status as $status
     | .message as $message
@@ -30,5 +43,8 @@ ls ${JSON_PATH}/*.json | xargs jq -c '[ .[]
     | .test_method as $method
     | input_filename as $file
     | { "file": $id,  "line": 1, "message": $message, "annotation_level": "failure" }
-]' | jq 'reduce inputs as $i (.; . += $i)' > ${ANNOTATIONS_FILE}
+]')
 
+cd $DIR
+
+jq 'reduce inputs as $i (.; . += $i)' <(echo "${SUMMARY}") <(echo "${FAILURES}") > ${ANNOTATIONS_FILE}
