f73f3be1e5cfe4d5631537a67e0e745396c9b03f
Kenneth Myhra <kennethmyhra@gmail.com>
2021-04-14 17:50:32 +0200

    fix+refactor: Binary requests can set Accept header (#373)
    * Binary GET requests are allowed to set Accept Header values which are
      outside the normal application/fhir+json, application/fhir+xml.
      A Binary GET request is allowed to set the Accept header to text/plain,
      application/pdf etc.
    * Refactored the Not Supported Format Response functionality. Moved it
      from FormatTypeHandler to UnsupportedMediaTypeHandler which is an
      IActionFilter
    * Issue #323, #360

--- src/Spark.Engine/Extensions/HttpRequestFhirExtensions.cs
+++ src/Spark.Engine/Extensions/HttpRequestFhirExtensions.cs
@@ -137,6 +137,13 @@ namespace Spark.Engine.Extensions
                 return false;
         }
 
+        internal static bool IsRawBinaryRequest(this HttpRequest request)
+        {
+            var ub = new UriBuilder(request.GetRequestUri());
+            return ub.Path.Contains("Binary")
+                && !ub.Path.EndsWith("_search");
+        }
+
         internal static bool IsRawBinaryPostOrPutRequest(this HttpRequest request)
         {
             var ub = new UriBuilder(request.GetRequestUri());
