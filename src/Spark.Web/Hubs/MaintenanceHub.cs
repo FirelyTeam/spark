0abee92a34181521415aca10e9d3de3ca5d501e2
Ole Kristian Losvik <losvik@gmail.com>
2021-04-14 11:15:48 +0200

    Fixing maintenance ui

--- src/Spark.Web/Hubs/MaintenanceHub.cs
+++ src/Spark.Web/Hubs/MaintenanceHub.cs
@@ -17,155 +17,137 @@ using Spark.Engine.Service.FhirServiceExtensions;
 
 namespace Spark.Web.Hubs
 {
-    public class MaintenanceHub : Hub
+  //[Authorize(Policy = "RequireAdministratorRole")]
+  public class MaintenanceHub : Hub
+  {
+    private List<Resource> _resources = null;
+
+    private IFhirService _fhirService;
+    private ILocalhost _localhost;
+    private IFhirStoreAdministration _fhirStoreAdministration;
+    private IFhirIndex _fhirIndex;
+    private ExamplesSettings _examplesSettings;
+    private IIndexRebuildService _indexRebuildService;
+    private readonly ILogger<MaintenanceHub> _logger;
+    private readonly IHubContext<MaintenanceHub> _hubContext;
+
+    private int _resourceCount;
+
+    public MaintenanceHub(
+        IFhirService fhirService,
+        ILocalhost localhost,
+        IFhirStoreAdministration fhirStoreAdministration,
+        IFhirIndex fhirIndex,
+        ExamplesSettings examplesSettings,
+        IIndexRebuildService indexRebuildService,
+        ILogger<MaintenanceHub> logger,
+        IHubContext<MaintenanceHub> hubContext)
     {
-        private List<Resource> _resources = null;
-
-        private IAsyncFhirService _fhirService;
-        private ILocalhost _localhost;
-        private IFhirStoreAdministration _fhirStoreAdministration;
-        private IFhirIndex _fhirIndex;
-        private ExamplesSettings _examplesSettings;
-        private IIndexRebuildService _indexRebuildService;
-        private readonly ILogger<MaintenanceHub> _logger;
-        private readonly IHubContext<MaintenanceHub> _hubContext;
-
-        private int _resourceCount;
-
-        public MaintenanceHub(
-            IAsyncFhirService fhirService,
-            ILocalhost localhost,
-            IFhirStoreAdministration fhirStoreAdministration,
-            IFhirIndex fhirIndex,
-            ExamplesSettings examplesSettings,
-            IIndexRebuildService indexRebuildService,
-            ILogger<MaintenanceHub> logger,
-            IHubContext<MaintenanceHub> hubContext)
-        {
-            _localhost = localhost;
-            _fhirService = fhirService;
-            _fhirStoreAdministration = fhirStoreAdministration;
-            _fhirIndex = fhirIndex;
-            _examplesSettings = examplesSettings;
-            _indexRebuildService = indexRebuildService;
-            _logger = logger;
-            _hubContext = hubContext;
-        }
-
-        public List<Resource> GetExampleData()
-        {
-            var list = new List<Resource>();
-            string examplePath = Path.Combine(AppContext.BaseDirectory, _examplesSettings.FilePath);
-
-            Bundle data;
-            data = FhirFileImport.ImportEmbeddedZip(examplePath).ToBundle();
-
-            if (data.Entry != null && data.Entry.Count() != 0)
-            {
-                foreach (var entry in data.Entry)
-                {
-                    if (entry.Resource != null)
-                    {
-                        list.Add((Resource)entry.Resource);
-                    }
-                }
-            }
-            return list;
-        }
-
-        private ImportProgressMessage Message(string message, int idx)
-        {
-            var msg = new ImportProgressMessage
-            {
-                Message = message,
-                Progress = (int)10 + (idx + 1) * 90 / _resourceCount
-            };
-            return msg;
-        }
-
-        public async void ClearStore()
-        {
-            var notifier = new HubContextProgressNotifier(_hubContext, _logger);
-            try
-            {
-                await notifier.SendProgressUpdate(0, "Clearing the database...");
-                await _fhirStoreAdministration.CleanAsync().ConfigureAwait(false);
-                await _fhirIndex.CleanAsync().ConfigureAwait(false);
-                await notifier.SendProgressUpdate(100, "Database cleared");
-            }
-            catch (Exception e)
-            {
-                await notifier.SendProgressUpdate(100, "ERROR CLEARING :( " + e.InnerException);
-            }
-
-        }
-
-        public async void RebuildIndex()
-        {
-            var notifier = new HubContextProgressNotifier(_hubContext, _logger);
-            try
-            {
-                await _indexRebuildService.RebuildIndexAsync(notifier)
-                    .ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Failed to rebuild index");
-
-                await notifier.SendProgressUpdate(100, "ERROR REBUILDING INDEX :( " + e.InnerException)
-                    .ConfigureAwait(false);
-            }
-        }
-
-        public async Tasks.Task LoadExamplesToStore()
-        {
-            var messages = new StringBuilder();
-            var notifier = new HubContextProgressNotifier(_hubContext, _logger);
-            try
-            {
-                await notifier.SendProgressUpdate(1, "Loading examples data...");
-                _resources = GetExampleData();
-
-                var resarray = _resources.ToArray();
-                _resourceCount = resarray.Count();
-
-                for (int x = 0; x <= _resourceCount - 1; x++)
-                {
-                    var res = resarray[x];
-                    // Sending message:
-                    var msg = Message("Importing " + res.TypeName + " " + res.Id + "...", x);
-                    await notifier.SendProgressUpdate(msg.Progress, msg.Message);
-
-                    try
-                    {
-                        Key key = res.ExtractKey();
-
-                        if (res.Id != null && res.Id != "")
-                        {
-                            await _fhirService.PutAsync(key, res).ConfigureAwait(false);
-                        }
-                        else
-                        {
-                            await _fhirService.CreateAsync(key, res).ConfigureAwait(false);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        // Sending message:
-                        var msgError = Message("ERROR Importing " + res.TypeName + " " + res.Id + "... ", x);
-                        await Clients.All.SendAsync("Error", msg);
-                        messages.AppendLine(msgError.Message + ": " + e.Message);
-                    }
-
-
-                }
-
-                await notifier.SendProgressUpdate(100, messages.ToString());
-            }
-            catch (Exception e)
-            {
-                await notifier.Progress("Error: " + e.Message);
-            }
-        }
+      _localhost = localhost;
+      _fhirService = fhirService;
+      _fhirStoreAdministration = fhirStoreAdministration;
+      _fhirIndex = fhirIndex;
+      _examplesSettings = examplesSettings;
+      _indexRebuildService = indexRebuildService;
+      _logger = logger;
+      _hubContext = hubContext;
     }
+
+    public List<Resource> GetExampleData()
+    {
+      var list = new List<Resource>();
+      string examplePath = Path.Combine(AppContext.BaseDirectory, _examplesSettings.FilePath);
+
+      Bundle data;
+      data = FhirFileImport.ImportEmbeddedZip(examplePath).ToBundle(_localhost.DefaultBase);
+
+      if (data.Entry != null && data.Entry.Count() != 0)
+      {
+        foreach (var entry in data.Entry)
+        {
+          if (entry.Resource != null)
+          {
+            list.Add((Resource)entry.Resource);
+          }
+        }
+      }
+      return list;
+    }
+
+    public async Task ClearStore()
+    {
+      try
+      {
+        await Clients.All.SendAsync("UpdateProgress", "Starting clearing database...");
+        _fhirStoreAdministration.Clean();
+        _fhirIndex.Clean();
+        await Clients.All.SendAsync("UpdateProgress", "Database cleared");
+      }
+      catch (Exception e)
+      {
+        await Clients.All.SendAsync("UpdateProgress", "ERROR CLEARING :( " + e.InnerException.Message);
+      }
+
+    }
+
+    public async Task RebuildIndex()
+    {
+      try
+      {
+        await Clients.All.SendAsync("UpdateProgress", "Rebuilding index...");
+        await _indexRebuildService.RebuildIndexAsync()
+            .ConfigureAwait(false);
+      }
+      catch (Exception e)
+      {
+        _logger.LogError(e, "Failed to rebuild index");
+
+        await Clients.All.SendAsync("UpdateProgress", "ERROR REBUILDING INDEX :( " + e.InnerException.Message)
+            .ConfigureAwait(false);
+      }
+    }
+
+    public async Task LoadExamplesToStore()
+    {
+      try
+      {
+        await Clients.All.SendAsync("UpdateProgress", "Loading examples");
+        _resources = GetExampleData();
+
+        var resarray = _resources.ToArray();
+        _resourceCount = resarray.Count();
+
+        for (int x = 0; x <= _resourceCount - 1; x++)
+        {
+          var res = resarray[x];
+          // Sending message:
+          var msg = $"Importing {res.ResourceType.ToString()} {res.Id} ...";
+          await Clients.All.SendAsync("UpdateProgress", msg);
+
+          try
+          {
+            Key key = res.ExtractKey();
+
+            if (res.Id != null && res.Id != "")
+            {
+              _fhirService.Put(key, res);
+            }
+            else
+            {
+              _fhirService.Create(key, res);
+            }
+          }
+          catch (Exception e)
+          {
+            var msgError = $"ERROR Importing {res.ResourceType.ToString()} {res.Id}... {e.InnerException.Message}";
+            await Clients.All.SendAsync("UpdateProgress", msgError);
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        await Clients.All.SendAsync("UpdateProgress", "Error: " + e.Message);
+      }
+    }
+  }
 }
