e1672b5747f0c375f1716e801242e0f3c51ced5f
Kenneth Myhra <kennethmyhra@gmail.com>
2021-09-05 12:20:48 +0200

    feature: Add GET interaction support for transaction bundles
    This adds support for GET interaction in batch/transaction bundles.
    
    Related to #305

---
+++ src/Spark.Engine/Service/FhirServiceExtensions/GetManipulationOperation.cs
@@ -0,0 +1,38 @@
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Spark.Engine.Core;
+using System;
+using System.Collections.Generic;
+
+namespace Spark.Engine.Service.FhirServiceExtensions
+{
+    public class GetManipulationOperation : ResourceManipulationOperation
+    {
+        public GetManipulationOperation(Resource resource, IKey operationKey, SearchResults searchResults, SearchParams searchCommand = null) 
+            : base(resource, operationKey, searchResults, searchCommand)
+        {
+        }
+        
+        public static Uri ReadSearchUri(Bundle.EntryComponent entry)
+        {
+            return entry.Request != null
+                ? new Uri(entry.Request.Url, UriKind.RelativeOrAbsolute)
+                : null;
+        }
+
+        protected override IEnumerable<Entry> ComputeEntries()
+        {
+            if (SearchResults != null)
+            {
+                foreach (string localKeyLiteral in SearchResults)
+                {
+                    yield return Entry.Create(Bundle.HTTPVerb.GET, Key.ParseOperationPath(localKeyLiteral));
+                }
+            }
+            else
+            {
+                yield return Entry.Create(Bundle.HTTPVerb.GET, OperationKey);
+            }
+        }
+    }
+}
