5b83da8a8d43afc868992936811dcf8754518108
Alexandr Shevchenko <shao-sss@hotmail.com>
2021-09-24 17:06:51 +0300

    Add Extensions support for patch functionality

--- src/Spark.Engine/Service/FhirServiceExtensions/PatchService.cs
+++ src/Spark.Engine/Service/FhirServiceExtensions/PatchService.cs
@@ -1,4 +1,5 @@
 ﻿using Hl7.Fhir.Introspection;
+using Hl7.Fhir.Utility;
 
 namespace Spark.Engine.Service.FhirServiceExtensions
 {
@@ -29,25 +30,24 @@ namespace Spark.Engine.Service.FhirServiceExtensions
                 var operationType = component.Part.First(x => x.Name == "type").Value.ToString();
                 var path = component.Part.First(x => x.Name == "path").Value.ToString();
                 var name = component.Part.FirstOrDefault(x => x.Name == "name")?.Value.ToString();
-                var value = (object)component.Part.FirstOrDefault(x => x.Name == "value")?.Value 
-                            ?? component.Part.FirstOrDefault(x => x.Name == "value")?.Part;
+                var valuePart = component.Part.FirstOrDefault(x => x.Name == "value") 
+                            ?? component.Part.FirstOrDefault(x => x.Name == "value");
 
                 var parameterExpression = Expression.Parameter(resource.GetType(), "x");
                 var expression = operationType == "add" ? _compiler.Parse($"{path}.{name}") : _compiler.Parse(path);
                 var result = expression.Accept(
                         new ResourceVisitor(parameterExpression),
                         new fhirExpression.SymbolTable());
-                var valueExpression = CreateValueExpression(value, result);
                 switch (operationType)
                 {
                     case "add":
-                        result = AddValue(result, valueExpression);
+                        result = AddValue(result, CreateValueExpression(valuePart, result.Type));
                         break;
                     case "insert":
-                        result = InsertValue(result, valueExpression);
+                        result = InsertValue(result, CreateValueExpression(valuePart, result.Type));
                         break;
                     case "replace":
-                        result = Expression.Assign(result, valueExpression);
+                        result = Expression.Assign(result, CreateValueExpression(valuePart, result.Type));
                         break;
                     case "delete":
                         result = DeleteValue(result);
@@ -65,39 +65,80 @@ namespace Spark.Engine.Service.FhirServiceExtensions
 
             return resource;
         }
-
-        private static Expression CreateValueExpression(object value, Expression result)
+        
+        private static Expression CreateValueExpression(Parameters.ParameterComponent part, Type resultType)
         {
-            Expression FromString(string str)
+            return part.Value == null
+                ? Expression.MemberInit(
+                    Expression.New(resultType.GenericTypeArguments[0].GetConstructor(Array.Empty<Type>())),
+                    GetPartsBindings(part.Part, resultType))
+                : GetConstantExpression(part.Value, resultType);
+        }
+        
+        private static Expression GetConstantExpression(DataType value, Type valueType)
+        {
+            Expression FromString(string str, Type targetType)
             {
-                return result is MemberExpression me
+                return targetType.CanBeTreatedAsType(typeof(DataType))
                     ? (Expression) Expression.MemberInit(
-                        Expression.New(me.Type.GetConstructor(Array.Empty<Type>())),
+                        Expression.New(targetType.GetConstructor(Array.Empty<Type>())),
                         Expression.Bind(
-                            me.Type.GetProperty("ObjectValue"),
+                            targetType.GetProperty("ObjectValue"),
                             Expression.Constant(str)))
-                    : Expression.Constant(value);
+                    : Expression.Constant(str);
             }
             
-            Expression FromParts(List<Parameters.ParameterComponent> parts)
-            {
-                return result.Type.IsGenericType
-                    ? (Expression) Expression.MemberInit(
-                        Expression.New(result.Type.GenericTypeArguments[0].GetConstructor(Array.Empty<Type>())),
-                        parts.Select(x => Expression.Bind(
-                            result.Type.GenericTypeArguments[0].GetProperties().Single(
-                                p => p.GetCustomAttribute<FhirElementAttribute>()?.Name == x.Name),
-                            Expression.Constant(x.Value))))
-                    : Expression.Constant(value);
-            }
-
             return value switch
             {
-                Code code => FromString(code.Value),
-                FhirString str => FromString(str.Value),
-                List<Parameters.ParameterComponent> parts => FromParts(parts),
+                Code code => FromString(code.Value, valueType == typeof(DataType) ? typeof(Code) : valueType),
+                FhirUri uir => FromString(uir.Value, valueType),
                 _ => Expression.Constant(value)
             };
+        }
+
+        private static IEnumerable<MemberBinding> GetPartsBindings(List<Parameters.ParameterComponent> parts, Type resultType)
+        {
+            foreach (var partGroup in parts.GroupBy(x => x.Name))
+            {
+                var property = resultType.GenericTypeArguments[0].GetProperties().Single(
+                    p => p.GetCustomAttribute<FhirElementAttribute>()?.Name == partGroup.Key);
+                if (property.PropertyType.IsGenericType)
+                {
+                    var listExpression = GetCollectionExpression(property, partGroup);
+                    yield return Expression.Bind(property, listExpression);
+                }
+                else
+                {
+                    var propertyValue = CreateValueExpression(partGroup.Single(), property.PropertyType);
+                    yield return Expression.Bind(property, propertyValue);
+                }
+            }
+        }
+
+        private static Expression GetCollectionExpression(PropertyInfo property, IEnumerable<Parameters.ParameterComponent> parts)
+        {
+            var variableExpr = Expression.Variable(property.PropertyType);
+            return Expression.Block(new [] {variableExpr}, GetCollectionCreationExpressions(variableExpr, property, parts));
+        }
+
+        private static IEnumerable<Expression> GetCollectionCreationExpressions(ParameterExpression variableExpr, PropertyInfo property, IEnumerable<Parameters.ParameterComponent> parts)
+        {
+            LabelTarget returnTarget = Expression.Label(property.PropertyType);
+            
+            GotoExpression returnExpression = Expression.Return(returnTarget, 
+                variableExpr, property.PropertyType);
+
+            LabelExpression returnLabel = Expression.Label(returnTarget, Expression.New(property.PropertyType.GetConstructor(Array.Empty<Type>())));
+
+            yield return Expression.Assign(variableExpr, Expression.New(property.PropertyType.GetConstructor(Array.Empty<Type>())));
+            foreach (var part in parts)
+            {
+                yield return Expression.Call(variableExpr, GetMethod(variableExpr.Type, "Add"),
+                    CreateValueExpression(part, property.PropertyType));
+            }
+
+            yield return returnExpression;
+            yield return returnLabel;
         }
 
         private static Expression MoveItem(Expression result, int source, int destination)
