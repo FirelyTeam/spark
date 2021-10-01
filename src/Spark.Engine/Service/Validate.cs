c88571e8fff342df5b664f6df553de2e45ff8195
Kenneth Myhra <kennethmyhra@gmail.com>
2021-10-01 14:23:09 +0200

    refactor: Add ValidateKey to static class Validate
    This adds the method ValidateKey to the static class Validate. It is
    currently also declared in FhirServiceBase.

--- src/Spark.Engine/Service/Validate.cs
+++ src/Spark.Engine/Service/Validate.cs
@@ -1,5 +1,6 @@
 ﻿/* 
  * Copyright (c) 2014, Furore (info@furore.com) and contributors
+ * Copyright (c) 2021, Incendi (info@incendi.no) and contributors
  * See the file CONTRIBUTORS for details.
  * 
  * This file is licensed under the BSD 3-Clause license
@@ -46,6 +47,20 @@ namespace Spark.Service
                     resource.TypeName, key.TypeName);
             }
 
+        }
+
+        public static void ValidateKey(IKey key, bool withVersion = false)
+        {
+            Validate.HasTypeName(key);
+            Validate.HasResourceId(key);
+            if (withVersion)
+            {
+                Validate.HasVersion(key);
+            }
+            else
+            {
+                Validate.HasNoVersion(key);
+            }
         }
 
         public static void Key(IKey key)
