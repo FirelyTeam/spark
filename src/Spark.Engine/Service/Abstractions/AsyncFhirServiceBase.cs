7c84d1ac63c319e065aeaf6bb7418d628f81f6bf
Kenneth Myhra <kennethmyhra@gmail.com>
2021-10-01 14:38:08 +0200

    refactor!: Add abstract classes FhirServiceBase and AsyncFhirServiceBase
    This adds two new abstract classes which inherits from IFhirService and
    IAsyncFhirService respectively. They let inheriting classes override the
    methods they want to use from underlying interfaces. By default the
    virtual methods in the base classes throws NotImplementedException.

---
+++ src/Spark.Engine/Service/Abstractions/AsyncFhirServiceBase.cs
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2021, Incendi (info@incendi.no) and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/FirelyTeam/spark/stu3/master/LICENSE
+ */
+
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Spark.Engine.Core;
+using Spark.Engine.FhirResponseFactory;
+using Spark.Engine.Service.FhirServiceExtensions;
+using Spark.Engine.Storage;
+using Spark.Service;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Spark.Engine.Service.Abstractions
+{
+    public class AsyncFhirServiceBase : ExtendableWith<IFhirServiceExtension>, IAsyncFhirService
+    {
+        protected readonly IFhirResponseFactory _responseFactory;
+        protected readonly ICompositeServiceListener _serviceListener;
+        
+        protected AsyncFhirServiceBase(IFhirServiceExtension[] extensions,
+            IFhirResponseFactory responseFactory,
+            ICompositeServiceListener serviceListener = null)
+        {
+            _responseFactory = responseFactory;
+            _serviceListener = serviceListener;
+
+            foreach (IFhirServiceExtension serviceExtension in extensions)
+            {
+                AddExtension(serviceExtension);
+            }
+        }
+
+        protected async Task<Entry> StoreAsync(Entry entry)
+        {
+            Entry result = await GetFeature<IResourceStorageService>()
+                .AddAsync(entry).ConfigureAwait(false);
+            await _serviceListener.InformAsync(entry).ConfigureAwait(false);
+            return result;
+        }
+
+        protected T GetFeature<T>() where T : IFhirServiceExtension
+        {
+            return FindExtension<T>() ??
+                   throw new NotSupportedException($"Feature {typeof(T)} not supported");
+        }
+
+        protected static void ValidateKey(IKey key, bool withVersion = false)
+        {
+            Validate.HasTypeName(key);
+            Validate.HasResourceId(key);
+            if (withVersion)
+            {
+                Validate.HasVersion(key);
+            }
+            else
+            {
+                Validate.HasNoVersion(key);
+            }
+            Validate.Key(key);
+        }
+        
+        public virtual Task<FhirResponse> AddMetaAsync(IKey key, Parameters parameters) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> ConditionalCreateAsync(IKey key, Resource resource, IEnumerable<Tuple<string, string>> parameters) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> ConditionalCreateAsync(IKey key, Resource resource, SearchParams parameters) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> ConditionalDeleteAsync(IKey key, IEnumerable<Tuple<string, string>> parameters) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> ConditionalUpdateAsync(IKey key, Resource resource, SearchParams parameters) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> CapabilityStatementAsync(string sparkVersion) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> CreateAsync(IKey key, Resource resource) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> DeleteAsync(IKey key) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> DeleteAsync(Entry entry) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> GetPageAsync(string snapshotKey, int index) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> HistoryAsync(HistoryParameters parameters) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> HistoryAsync(string type, HistoryParameters parameters) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> HistoryAsync(IKey key, HistoryParameters parameters) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> PutAsync(IKey key, Resource resource) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> PutAsync(Entry entry) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> ReadAsync(IKey key, ConditionalHeaderParameters parameters = null) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> ReadMetaAsync(IKey key) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> SearchAsync(string type, SearchParams searchCommand, int pageIndex = 0) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> TransactionAsync(IList<Entry> interactions) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> TransactionAsync(Bundle bundle) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> UpdateAsync(IKey key, Resource resource) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> PatchAsync(IKey key, Parameters patch) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> ValidateOperationAsync(IKey key, Resource resource) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> VersionReadAsync(IKey key) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> VersionSpecificUpdateAsync(IKey versionedKey, Resource resource) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> EverythingAsync(IKey key) => throw new NotImplementedException();
+        public virtual Task<FhirResponse> DocumentAsync(IKey key) => throw new NotImplementedException();
+    }
+}
