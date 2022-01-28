32bbe2435ba67682eadb00a8ed7ab5f0fa6eff46
Kenneth Myhra <kennethmyhra@gmail.com>
2022-01-28 13:08:28 +0100

    PatchService: Add license header

--- src/Spark.Engine.Test/Service/PatchServiceTests.cs
+++ src/Spark.Engine.Test/Service/PatchServiceTests.cs
@@ -1,4 +1,12 @@
-﻿using Hl7.Fhir.Model;
+﻿/*
+ * Copyright (c) 2022, Incendi (info@incendi.no) and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/FirelyTeam/spark/stu3/master/LICENSE
+ */
+
+using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
 using Hl7.Fhir.Serialization;
 using Spark.Engine.Service.FhirServiceExtensions;
