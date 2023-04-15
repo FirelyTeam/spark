1af661e29214e3d29ab2ca416980ccb4a3ef4fa4
Kenneth Myhra <kennethmyhra@gmail.com>
2023-04-15 19:24:30 +0200

    Engine: Extract and rename extension method ExtractMeta()
    Extracted ExtractMeta() into ParametersExtensions.cs, and renamed it to
    ExtractMetaResources().

---
+++ src/Spark.Engine/Extensions/ParametersExtensions.cs
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023, Incendi (info@incendi.no) and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.github.com/furore-fhir/spark/master/LICENSE
+ */
+
+using Hl7.Fhir.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spark.Engine.Extensions
+{
+    public static class ParametersExtensions
+    {
+        public static IEnumerable<Meta> ExtractMetaResources(this Parameters parameters)
+        {
+            foreach(var parameter in parameters.Parameter.Where(p => p.Name == "meta"))
+            {
+                if (parameter.Value is Meta meta)
+                    yield return meta;
+            }
+        }
+    }
+}
