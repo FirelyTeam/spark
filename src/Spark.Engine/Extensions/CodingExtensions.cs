207eda6efcc27089a22c88066eedd704b9f97c21
Kenneth Myhra <kennethmyhra@gmail.com>
2023-04-15 19:36:36 +0200

    Engine: Add extension methods for the Coding data type
    This adds two extension methods:
    - AreEqual(this Coding, Coding other)
      - Checks if the two Coding instances have identical System and Code.
    - HasEqualCoding(this IEnumerable<Coding>, Coding other)
      - Compares the Coding list of instances against other using the above
        method AreEqual() to see if they are equal.

---
+++ src/Spark.Engine/Extensions/CodingExtensions.cs
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023, Incendi (info@incendi.no) and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.github.com/furore-fhir/spark/master/LICENSE
+ */
+
+using Hl7.Fhir.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spark.Engine.Extensions
+{
+    public static class CodingExtensions
+    {
+        /// <summary>
+        /// Compares this <see cref="Coding"/> instance against other, returns true if the two have identical System and
+        /// Code, otherwise false.
+        /// </summary>
+        /// <param name="coding">This <see cref="Coding"/> instance.</param>
+        /// <param name="other">The <see cref="Coding"/> instance to compare this against.</param>
+        /// <returns></returns>
+        public static bool AreEqual(this Coding coding, Coding other)
+        {
+            return coding.System == other.System && coding.Code == other.Code;
+        }
+
+        /// <summary>
+        /// Compares this list of <see cref="Coding"/> Coding instances against the <see cref="Coding"/> instance other,
+        /// returns true if at least one <see cref="Coding"/> instance have identical System and Code to other, otherwise false.
+        /// </summary>
+        /// <param name="sources">This list of <see cref="Coding"/> instances.</param>
+        /// <param name="other">The <see cref="Coding"/> instance to compare this list against.</param>
+        /// <returns></returns>
+        public static bool HasEqualCoding(this IEnumerable<Coding> sources, Coding other)
+        {
+            return sources.Any(source => AreEqual(source, other));
+        }
+    }
+}
