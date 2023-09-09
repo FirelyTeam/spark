fd096df3682f6fc4029abcb3902efa8ccc145a3c
Kenneth Myhra <kennethmyhra@gmail.com>
2023-09-09 08:20:07 +0200

    Mongo: Add GuidGenerator a new unique identity generator
    This is supposed to be a replacement for MongoIdGenerator.

---
+++ src/Spark.Mongo/Store/GuidGenerator.cs
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023, Incendi (info@incendi.no) and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/FirelyTeam/spark/stu3/master/LICENSE
+ */
+
+using Hl7.Fhir.Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Spark.Core;
+using Spark.Store.Mongo;
+using System;
+
+namespace Spark.Mongo.Store
+{
+    public class GuidGenerator : IGenerator
+    {
+        private readonly IMongoDatabase _database;
+        private readonly string _formatSpecifier;
+
+        public GuidGenerator(string mongoUrl, string formatSpecifier = "D")
+        {
+            _database = MongoDatabaseFactory.GetMongoDatabase(mongoUrl);
+            _formatSpecifier = formatSpecifier;
+        }
+        public string NextResourceId(Resource resource)
+        {
+            var id = Guid.NewGuid().ToString(_formatSpecifier);
+            return id;
+        }
+
+        public string NextVersionId(string resourceIdentifier) => throw new NotImplementedException();
+
+        public string NextVersionId(string resourceType, string resourceIdentifier)
+        {
+            var name = resourceType + "_history_" + resourceIdentifier;
+            var versionId = Next(name);
+            return versionId;
+        }
+
+        private string Next(string name)
+        {
+            var query = Builders<BsonDocument>.Filter.Eq(Field.PRIMARYKEY, name);
+            var update = Builders<BsonDocument>.Update.Inc(Field.COUNTERVALUE, 1);
+            var options = new FindOneAndUpdateOptions<BsonDocument>
+            {
+                IsUpsert = true,
+                ReturnDocument = ReturnDocument.After,
+                Projection = Builders<BsonDocument>.Projection.Include(Field.COUNTERVALUE)
+            };
+
+            var collection = _database.GetCollection<BsonDocument>(Collection.COUNTERS);
+            var document = collection.FindOneAndUpdate(query, update, options);
+
+            return document[Field.COUNTERVALUE].AsInt32.ToString();
+        }
+    }
+}
