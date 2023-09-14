0ea9888a16e7266dc882b86ff5171f41d4c5f55d
Kenneth Myhra <kennethmyhra@gmail.com>
2023-09-14 17:44:30 +0200

    Spark-Legacy: Remove Spark-Legacy the original Spark UI
    This removes Spark-Legacy the original Spark UI, this is a .NET
    Framework 4.6.* web application which is no longer actively maintained
    and has been replaced by the ASP.NET Core web application Spark.Web.

--- src/Spark-Legacy/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Spark.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
