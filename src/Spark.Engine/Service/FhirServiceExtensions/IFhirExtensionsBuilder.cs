3dd560baeaa0397c5f8be4620e0882b984694300
Kenneth Myhra <kennethmyhra@gmail.com>
2023-10-27 20:20:49 +0200

    Engine: Delete unused {I,}FhirExtensionsBuilder

--- src/Spark.Engine/Service/FhirServiceExtensions/IFhirExtensionsBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Spark.Engine.Service.FhirServiceExtensions
-{
-    public interface IFhirExtensionsBuilder : IEnumerable<IFhirServiceExtension>
-    {
-        IEnumerable<IFhirServiceExtension> GetExtensions();
-    }
-}
