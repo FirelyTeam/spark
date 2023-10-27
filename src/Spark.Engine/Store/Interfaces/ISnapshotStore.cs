7668840f360c608b94dc00076a9d9519163bea1c
Kenneth Myhra <kennethmyhra@gmail.com>
2023-10-27 21:08:22 +0200

    Engine: Remove non-async methods ISnapshotStore and MongoSnapshotStore

--- src/Spark.Engine/Store/Interfaces/ISnapshotStore.cs
+++ src/Spark.Engine/Store/Interfaces/ISnapshotStore.cs
@@ -1,6 +1,6 @@
 /* 
  * Copyright (c) 2014, Furore (info@furore.com) and contributors
- * Copyright (c) 2021, Incendi (info@incendi.no) and contributors
+ * Copyright (c) 2021-2023, Incendi (info@incendi.no) and contributors
  * See the file CONTRIBUTORS for details.
  * 
  * This file is licensed under the BSD 3-Clause license
@@ -14,10 +14,7 @@ namespace Spark.Engine.Store.Interfaces
 {
     public interface ISnapshotStore
     {
-        void AddSnapshot(Snapshot snapshot);
         Task AddSnapshotAsync(Snapshot snapshot);
-
-        Snapshot GetSnapshot(string snapshotId);
         Task<Snapshot> GetSnapshotAsync(string snapshotId);
     }
 }
--- src/Spark.Mongo/Store/MongoSnapshotStore.cs
+++ src/Spark.Mongo/Store/MongoSnapshotStore.cs
@@ -1,6 +1,6 @@
 ﻿/* 
  * Copyright (c) 2014, Furore (info@furore.com) and contributors
- * Copyright (c) 2021, Incendi (info@incendi.no) and contributors
+ * Copyright (c) 2021-2023, Incendi (info@incendi.no) and contributors
  * See the file CONTRIBUTORS for details.
  * 
  * This file is licensed under the BSD 3-Clause license
@@ -24,22 +24,10 @@ namespace Spark.Mongo.Store
             _database = MongoDatabaseFactory.GetMongoDatabase(mongoUrl);
         }
 
-        public void AddSnapshot(Snapshot snapshot)
-        {
-            var collection = _database.GetCollection<Snapshot>(Collection.SNAPSHOT);
-            collection.InsertOne(snapshot);
-        }
-
         public async Task AddSnapshotAsync(Snapshot snapshot)
         {
             var collection = _database.GetCollection<Snapshot>(Collection.SNAPSHOT);
             await collection.InsertOneAsync(snapshot).ConfigureAwait(false);
-        }
-
-        public Snapshot GetSnapshot(string snapshotId)
-        {
-            var collection = _database.GetCollection<Snapshot>(Collection.SNAPSHOT);
-            return collection.Find(s => s.Id == snapshotId).FirstOrDefault();
         }
 
         public async Task<Snapshot> GetSnapshotAsync(string snapshotId)
