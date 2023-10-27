7c815e836fa5a550740116ad2864836f6657c371
Kenneth Myhra <kennethmyhra@gmail.com>
2023-10-27 21:35:43 +0200

    Engine: Remove non-async methods on {IFhir,Mongo}StoreAdministration

--- src/Spark.Engine/Interfaces/IFhirStoreAdministration.cs
+++ src/Spark.Engine/Interfaces/IFhirStoreAdministration.cs
@@ -1,20 +1,18 @@
-﻿/* 
+﻿/*
  * Copyright (c) 2016, Furore (info@furore.com) and contributors
- * Copyright (c) 2021, Incendi (info@incendi.no) and contributors
+ * Copyright (c) 2021-2023, Incendi (info@incendi.no) and contributors
  * See the file CONTRIBUTORS for details.
- * 
+ *
  * This file is licensed under the BSD 3-Clause license
  * available at https://raw.githubusercontent.com/FirelyTeam/spark/stu3/master/LICENSE
  */
 
-using System;
 using System.Threading.Tasks;
 
 namespace Spark.Engine.Interfaces
 {
     public interface IFhirStoreAdministration
     {
-        void Clean();
         Task CleanAsync();
     }
 }
