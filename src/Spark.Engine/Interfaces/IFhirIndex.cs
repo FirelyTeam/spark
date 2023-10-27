1d7b03eeb4347c2542fac45d1712312482eed579
Kenneth Myhra <kennethmyhra@gmail.com>
2023-10-27 21:14:26 +0200

    Engine: Remove non-async methods on IFhirIndex and MongoFhirIndex

--- src/Spark.Engine/Interfaces/IFhirIndex.cs
+++ src/Spark.Engine/Interfaces/IFhirIndex.cs
@@ -1,13 +1,12 @@
-﻿/* 
+﻿/*
  * Copyright (c) 2014, Furore (info@furore.com) and contributors
  * Copyright (c) 2021, Incendi (info@incendi.no) and contributors
  * See the file CONTRIBUTORS for details.
- * 
+ *
  * This file is licensed under the BSD 3-Clause license
  * available at https://raw.githubusercontent.com/FirelyTeam/spark/stu3/master/LICENSE
  */
 
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Hl7.Fhir.Rest;
@@ -15,19 +14,11 @@ using Spark.Engine.Core;
 
 namespace Spark.Core
 {
-
     public interface IFhirIndex
     {
-        void Clean();
         Task CleanAsync();
-
-        SearchResults Search(string resource, SearchParams searchCommand);
         Task<SearchResults> SearchAsync(string resource, SearchParams searchCommand);
-
-        Key FindSingle(string resource, SearchParams searchCommand);
         Task<Key> FindSingleAsync(string resource, SearchParams searchCommand);
-
-        SearchResults GetReverseIncludes(IList<IKey> keys, IList<string> revIncludes);
         Task<SearchResults> GetReverseIncludesAsync(IList<IKey> keys, IList<string> revIncludes);
     }
 }
