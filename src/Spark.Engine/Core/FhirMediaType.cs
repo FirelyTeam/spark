17d030e02498c8098cc1ec1b41c6e1abc29a4820
whyfate <593301179@qq.com>
2023-12-14 08:43:56 +0000

    Engine+Tests+Web: Enable support for non-FHIR APIs
    This enables supports for adding endpoints/controllers for non-FHIR
    APIs.

--- src/Spark.Engine/Core/FhirMediaType.cs
+++ src/Spark.Engine/Core/FhirMediaType.cs
@@ -23,12 +23,13 @@ public static class FhirMediaType
     public static string OctetStreamMimeType = "application/octet-stream";
     public static string FormUrlEncodedMimeType = "application/x-www-form-urlencoded";
     public static string AnyMimeType = "*/*";
+    public static string MultipartFormDataMimeType = "multipart/form-data";
 
     public static IEnumerable<string> JsonMimeTypes => ContentType.JSON_CONTENT_HEADERS;
     public static IEnumerable<string> XmlMimeTypes => ContentType.XML_CONTENT_HEADERS;
     public static IEnumerable<string> SupportedMimeTypes => JsonMimeTypes
         .Concat(XmlMimeTypes)
-        .Concat(new[] { OctetStreamMimeType, FormUrlEncodedMimeType, AnyMimeType });
+        .Concat(new[] { OctetStreamMimeType, FormUrlEncodedMimeType, AnyMimeType, MultipartFormDataMimeType });
 
     /// <summary>
     /// Transforms loose formats to their strict variant
