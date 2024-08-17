e30ff4bdb923ae06e441b1c46fbcf5a9c7e7a41a
Kenneth Myhra <kennethmyhra@gmail.com>
2024-08-17 22:22:26 +0200

    Engine+Mongo+Tests: Remove a few unused classes and interfaces

--- src/Spark.Engine/Store/Interfaces/IFhirStoreExtension.cs
+++
@@ -1,9 +0,0 @@
-using Spark.Engine.Core;
-
-namespace Spark.Engine.Store.Interfaces
-{
-    public interface IFhirStoreExtension : IExtension<IFhirStore>
-    {
-        void OnEntryAdded(Entry entry);
-    }
-}
