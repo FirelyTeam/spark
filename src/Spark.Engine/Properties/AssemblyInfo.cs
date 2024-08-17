fbc00efbe935066c73a46bbfc922d08201b0b6bc
Kenneth Myhra <kennethmyhra@gmail.com>
2024-08-17 20:25:28 +0200

    Engine: Remove unnecessary class AssemblyInfo

--- src/Spark.Engine/Properties/AssemblyInfo.cs
+++ src/Spark.Engine/Properties/AssemblyInfo.cs
@@ -10,10 +10,3 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Spark.Engine.Test")]
-
-namespace Spark.Engine.Properties
-{
-    internal class AssemblyInfo
-    {
-    }
-}
