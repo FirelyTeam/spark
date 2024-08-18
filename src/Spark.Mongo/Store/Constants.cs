fefab0c9186e057c30e6bd1384b4c19ffef13fc2
Kenneth Myhra <kennethmyhra@gmail.com>
2024-08-18 08:33:14 +0200

    Mongo: Remove unused constans

--- src/Spark.Mongo/Store/Constants.cs
+++ src/Spark.Mongo/Store/Constants.cs
@@ -43,8 +43,5 @@ namespace Spark.Store.Mongo
     {
         public const string CURRENT = "current";
         public const string SUPERCEDED = "superceded";
-        internal const string QUEUED = "queued";
-        public const string IDPREFIX = "nl.furore.spark.";
-        public const string VIDPREFIX = "h";
     }
 }
