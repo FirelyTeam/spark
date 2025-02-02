497be443590780d20861162a145ceb1a01b7847b
Kenneth Myhra <kennethmyhra@gmail.com>
2025-02-02 10:27:39 +0100

    Engine+Mongo: Drop target for netstandard2.0
    This drops the netstandard2.1 target and targets netstandard2.1 instead.

--- src/Spark.Engine/Maintenance/MaintenanceModeHandler.cs
+++ src/Spark.Engine/Maintenance/MaintenanceModeHandler.cs
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#if NETSTANDARD2_0 || NET6_0_OR_GREATER
+#if NETSTANDARD2_1 || NET6_0_OR_GREATER
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
