13c9716f4c07a7263820ed072319b7a74bd98c83
Kenneth Myhra <kennethmyhra@gmail.com>
2025-02-03 21:05:56 +0100

    Mongo: Make InternalField class and constants internal

--- src/Spark.Mongo/Search/Common/Config.cs
+++ src/Spark.Mongo/Search/Common/Config.cs
@@ -9,17 +9,17 @@ using System;
 
 namespace Spark.Mongo.Search.Common;
 
-public static class InternalField
+internal static class InternalField
 {
     // Internally stored search fields
-    public const string ID = "internal_id";
-    public const string JUST_ID = "internal_justid";
-    public const string LAST_UPDATED = "lastupdated";
-    public const string LEVEL = "internal_level";
-    public const string RESOURCE = "internal_resource";
-    public const string SELF_LINK = "internal_selflink";
+    internal const string ID = "internal_id";
+    internal const string JUST_ID = "internal_justid";
+    internal const string LAST_UPDATED = "lastupdated";
+    internal const string LEVEL = "internal_level";
+    internal const string RESOURCE = "internal_resource";
+    internal const string SELF_LINK = "internal_selflink";
 
-    public static string[] ALL = { ID, JUST_ID, SELF_LINK, RESOURCE, LEVEL, LAST_UPDATED };
+    internal static string[] ALL = { ID, JUST_ID, SELF_LINK, RESOURCE, LEVEL, LAST_UPDATED };
 }
 
 public static class UniversalField
