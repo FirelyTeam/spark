508a9705b4c8c038f5f099d9a3fb7dad0012f1d1
Kenneth Myhra <kennethmyhra@gmail.com>
2025-02-03 20:00:23 +0100

    Engine: Add generic versions of {,I}FhirResponseFactory.GetFhirResponse
    Adds the generic methods {,I}FhirResponseFactory.GetFhirResponse<T>()
    where T is of Resource.

--- src/Spark.Engine/FhirResponseFactory/IFhirResponseFactory.cs
+++ src/Spark.Engine/FhirResponseFactory/IFhirResponseFactory.cs
@@ -1,20 +1,24 @@
-﻿/* 
+﻿/*
  * Copyright (c) 2016-2018, Firely <info@fire.ly>
  * Copyright (c) 2019-2025, Incendi <info@incendi.no>
- * 
+ *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-using System;
-using System.Collections.Generic;
 using Hl7.Fhir.Model;
 using Spark.Engine.Core;
+using System;
+using System.Collections.Generic;
 
 namespace Spark.Engine.FhirResponseFactory;
 
 public interface IFhirResponseFactory
 {
+    FhirResponse<T> GetFhirResponse<T>(Entry entry, IKey key = null, IEnumerable<object> parameters = null)
+        where T : Resource;
     FhirResponse GetFhirResponse(Entry entry, IKey key = null, IEnumerable<object> parameters = null);
+    FhirResponse<T> GetFhirResponse<T>(Entry entry, IKey key = null, params object[] parameters)
+        where T : Resource;
     FhirResponse GetFhirResponse(Entry entry, IKey key = null, params object[] parameters);
     FhirResponse GetMetadataResponse(Entry entry, IKey key = null);
     FhirResponse GetFhirResponse(IList<Entry> interactions, Bundle.BundleType bundleType);
