b7c9e1e4c2aefb88cd00a30f70e15360848f2feb
Kenneth Myhra <kennethmyhra@gmail.com>
2025-02-03 19:56:19 +0100

    Engine: Add a generic version of FhirResponse
    This adds a generic version of FhirResponse. FhirResponse<T> where T is
    Resource.

--- src/Spark.Engine/Core/Response.cs
+++ src/Spark.Engine/Core/Response.cs
@@ -1,7 +1,7 @@
-﻿/* 
+﻿/*
  * Copyright (c) 2015-2018, Firely <info@fire.ly>
  * Copyright (c) 2021-2025, Incendi <info@incendi.no>
- * 
+ *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -13,6 +13,50 @@ namespace Spark.Engine.Core;
 // This way, it's easier to implement multiple WebApi controllers
 // without having to implement functionality twice.
 // The FhirService always responds with a "Response"
+
+public class FhirResponse<T> where T : Resource
+{
+    public HttpStatusCode StatusCode { get; }
+    public IKey Key { get; }
+    public T Resource { get; }
+
+    public FhirResponse(HttpStatusCode code, IKey key, T resource)
+    {
+        StatusCode = code;
+        Key = key;
+        Resource = resource;
+    }
+
+    public FhirResponse(HttpStatusCode code, T resource)
+    {
+        StatusCode = code;
+        Key = null;
+        Resource = resource;
+    }
+
+    public FhirResponse(HttpStatusCode code)
+    {
+        StatusCode = code;
+    }
+
+    public bool IsValid
+    {
+        get
+        {
+            int code = (int)StatusCode;
+            return code <= 300;
+        }
+    }
+
+    public bool HasBody => Resource != null;
+
+    public override string ToString()
+    {
+        string details = Resource != null ? string.Format("({0})", Resource.TypeName) : null;
+        string location = Key?.ToString();
+        return string.Format("{0}: {1} {2} ({3})", (int)StatusCode, StatusCode.ToString(), details, location);
+    }
+}
 
 public class FhirResponse
 {
