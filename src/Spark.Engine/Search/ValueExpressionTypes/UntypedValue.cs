9ea25d4a430319f1427e58947694bf6f7a7e6c19
Kenneth Myhra <kennethmyhra@gmail.com>
2025-02-05 21:40:46 +0100

    Engine: Modernize and clean up value expression type UntypedValue

--- src/Spark.Engine/Search/ValueExpressionTypes/UntypedValue.cs
+++ src/Spark.Engine/Search/ValueExpressionTypes/UntypedValue.cs
@@ -1,6 +1,6 @@
-﻿/* 
+﻿/*
  * Copyright (c) 2015-2018, Firely <info@fire.ly>
- * 
+ *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -10,50 +10,26 @@ namespace Spark.Search;
 
 public class UntypedValue : ValueExpression
 {
-    public string Value { get; private set; }
-
     public UntypedValue(string value)
     {
         Value = value;
     }
 
-    public override string ToString()
-    {
-        return Value;
-    }
+    public string Value { get; }
 
-    public NumberValue AsNumberValue()
-    {
-        return NumberValue.Parse(Value);
-    }
+    public override string ToString() => Value;
 
-    public DateValue AsDateValue()
-    {
-        return DateValue.Parse(Value);
-    }
+    public NumberValue AsNumberValue() => NumberValue.Parse(Value);
 
-    public FhirDateTime AsDateTimeValue()
-    {
-        return new FhirDateTime(Value);
-    }
+    public DateValue AsDateValue() => DateValue.Parse(Value);
 
-    public StringValue AsStringValue()
-    {
-        return StringValue.Parse(Value);
-    }
+    public FhirDateTime AsDateTimeValue() => new(Value);
 
-    public TokenValue AsTokenValue()
-    {
-        return TokenValue.Parse(Value);
-    }
+    public StringValue AsStringValue() => StringValue.Parse(Value);
 
-    public QuantityValue AsQuantityValue()
-    {
-        return QuantityValue.Parse(Value);
-    }
+    public TokenValue AsTokenValue() => TokenValue.Parse(Value);
 
-    public ReferenceValue AsReferenceValue()
-    {
-        return ReferenceValue.Parse(Value);
-    }
+    public QuantityValue AsQuantityValue() => QuantityValue.Parse(Value);
+
+    public ReferenceValue AsReferenceValue() => ReferenceValue.Parse(Value);
 }
