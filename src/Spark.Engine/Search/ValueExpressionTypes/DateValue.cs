bac2aecbc48575060e2ba29223b74c7541f4bca2
Kenneth Myhra <kennethmyhra@gmail.com>
2025-02-05 21:32:22 +0100

    Engine: Modernize and clean up value expression type DateValue

--- src/Spark.Engine/Search/ValueExpressionTypes/DateValue.cs
+++ src/Spark.Engine/Search/ValueExpressionTypes/DateValue.cs
@@ -1,7 +1,7 @@
-﻿/* 
+﻿/*
  * Copyright (c) 2015-2018, Firely <info@fire.ly>
  * Copyright (c) 2020-2025, Incendi <info@incendi.no>
- * 
+ *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
@@ -13,8 +13,6 @@ namespace Spark.Search;
 
 public class DateValue : ValueExpression
 {
-    public string Value { get; private set; }
-
     public DateValue(DateTimeOffset value)
     {
         // The DateValue datatype is not interested in any time related
@@ -28,21 +26,19 @@ public class DateValue : ValueExpression
         if (!Date.IsValidValue(date))
         {
             if (!FhirDateTime.IsValidValue(date))
-                throw Error.Argument("date", "The string [" + date + "] is not a valid FHIR date string and isn't a FHIR datetime either");
-                
+                throw Error.Argument("date",
+                    $"The string [{date}] is not a valid FHIR date string and isn't a FHIR datetime either");
+
             // This was a time, so we can just use the date portion of this
-            date = (new FhirDateTime(date)).ToDateTimeOffset(TimeSpan.Zero).Date.ToString("yyyy-MM-dd");
+            date = new FhirDateTime(date).ToDateTimeOffset(TimeSpan.Zero).Date.ToString("yyyy-MM-dd");
         }
+
         Value = date;
     }
 
-    public override string ToString()
-    {
-        return Value;
-    }
+    public string Value { get; }
 
-    public static DateValue Parse(string text)
-    {
-        return new DateValue(text);
-    }
+    public override string ToString() => Value;
+
+    public static DateValue Parse(string text) => new(text);
 }
