a34e9341d62ad239c5d87c3127f7f4ce0d0ed229
Kenneth Myhra <kennethmyhra@gmail.com>
2025-02-05 21:33:11 +0100

    Engine: Modernize and clean up value expression type NumberValue

--- src/Spark.Engine/Search/ValueExpressionTypes/NumberValue.cs
+++ src/Spark.Engine/Search/ValueExpressionTypes/NumberValue.cs
@@ -1,30 +1,23 @@
-﻿/* 
+﻿/*
  * Copyright (c) 2015-2018, Firely <info@fire.ly>
- * 
+ *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 using Hl7.Fhir.Serialization;
-using System;
 
 namespace Spark.Search;
 
 public class NumberValue : ValueExpression
 {
-    public Decimal Value { get; private set; }
-     
-    public NumberValue(Decimal value)
+    public NumberValue(decimal value)
     {
         Value = value;
     }
-                              
-    public override string ToString()
-    {
-        return PrimitiveTypeConverter.ConvertTo<string>(Value);
-    }
 
-    public static NumberValue Parse(string text)
-    {
-        return new NumberValue(PrimitiveTypeConverter.ConvertTo<Decimal>(text));
-    }
+    public decimal Value { get; }
+
+    public override string ToString() => PrimitiveTypeConverter.ConvertTo<string>(Value);
+
+    public static NumberValue Parse(string text) => new(PrimitiveTypeConverter.ConvertTo<decimal>(text));
 }
