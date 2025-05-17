144a7103bebd6e01b49ac4c80a814d50965e4aad
Kenneth Myhra <kennethmyhra@gmail.com>
2025-05-17 20:52:41 +0200

    Engine: Null check currentReference and early return to avoid nesting

--- src/Spark.Engine/Service/FhirServiceExtensions/IndexService.cs
+++ src/Spark.Engine/Service/FhirServiceExtensions/IndexService.cs
@@ -153,14 +153,14 @@ public class IndexService : IIndexService
                 // Replace references to these contained resources with the newly created id's.
                 ResourceVisitor.VisitByType(
                     domainResource,
-                    (el, path) => {
-                        ResourceReference currentRefence = (el as ResourceReference);
-                        if (!string.IsNullOrEmpty(currentRefence.Reference))
-                        {
-                            referenceMap.TryGetValue(currentRefence.Reference, out string replacementId);
-                            if (replacementId != null)
-                                currentRefence.Reference = replacementId;
-                        }
+                    (element, _) => {
+                        ResourceReference currentRefence = (element as ResourceReference);
+                        if (string.IsNullOrEmpty(currentRefence?.Reference))
+                            return;
+
+                        referenceMap.TryGetValue(currentRefence.Reference, out string replacementId);
+                        if (replacementId != null)
+                            currentRefence.Reference = replacementId;
                     },
                     typeof(ResourceReference));
             }
