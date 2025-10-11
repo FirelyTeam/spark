f9045fe147f9b099896096ee1809c25d7092def4
Kenneth Myhra <kennethmyhra@gmail.com>
2025-10-11 13:22:41 +0200

    Tests: Add [assembly: Parallelize()] to parallelize tests

---
+++ src/Spark.Web.Tests/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2025, Incendi <info@incendi.no>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: Parallelize(Scope = ExecutionScope.ClassLevel)]
