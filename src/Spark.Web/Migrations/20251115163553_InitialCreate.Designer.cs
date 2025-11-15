144345b5cf0f1df740b60a30392df75f1f12e3bd
Kenneth Myhra <kennethmyhra@gmail.com>
2025-11-15 17:45:48 +0100

    Spark.Web: Avoid dynamic values in ApplicationDbContext.HasData()
    Adds a constant Id, and ConcurrenyStamp for the default user, also
    recreates the EF migrations.

--- src/Spark.Web/Migrations/20251115163553_InitialCreate.Designer.cs
+++ src/Spark.Web/Migrations/20251115163553_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Spark.Web.Data;
 
@@ -10,9 +11,11 @@ using Spark.Web.Data;
 namespace Spark.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251115163553_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
