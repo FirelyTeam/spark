9fe2aea30e4ab172d1f03975d718b5cfdec9c44a
Kenneth Myhra <kennethmyhra@gmail.com>
2025-11-22 09:05:47 +0100

    Spark.Web: Move away from IWebHostBuilder and use IHostBuilder instead
    In net10.0 IWebHostBuilder is marked as obsolete.
    
    Also refactor the startup sequence, by building a default IConfiguration
    from our new helper class AppSettings + moves the database seeding into
    Startup.Configure().

--- src/Spark.Web/Program.cs
+++ src/Spark.Web/Program.cs
@@ -4,15 +4,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-using System;
-using System.IO;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Spark.Web.Data;
+using Spark.Web.Settings;
 
 namespace Spark.Web;
 
@@ -20,38 +15,23 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var host = CreateWebHostBuilder(args).Build();
-        var config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false)
-            .AddEnvironmentVariables()
-            .AddUserSecrets<Startup>()
-            .Build();
-
-        using (var scope = host.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            try
-            {
-                var context = services.GetRequiredService<ApplicationDbContext>();
-                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
-                ApplicationDbInitializer.SeedAdmin(context, userManager, config);
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occurred while seeding the database.");
-            }
-        }
-
-        host.Run();
+        CreateWebHostBuilder(args)
+            .Build()
+            .Run();
     }
 
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
+    private static IHostBuilder CreateWebHostBuilder(string[] args)
+    {
+        var configuration = AppSettings.BuildDefaultConfiguration();
+        return Host.CreateDefaultBuilder(args)
             .ConfigureLogging(logging =>
             {
                 logging.AddConsole();
+            })
+            .ConfigureWebHostDefaults(webHostBuilder =>
+            {
+                webHostBuilder.UseStartup<Startup>();
+                webHostBuilder.UseConfiguration(configuration);
             });
+    }
 }
