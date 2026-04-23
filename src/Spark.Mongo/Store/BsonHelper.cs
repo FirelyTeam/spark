5d6d3aac8da81fb9213f9452bbe108d5c11943e1
Kenneth Myhra <kennethmyhra@gmail.com>
2026-04-23 21:33:32 +0200

    Mongo: Move IKey-related methods into IKeyExtensions

--- src/Spark.Mongo/Store/BsonHelper.cs
+++ src/Spark.Mongo/Store/BsonHelper.cs
@@ -5,7 +5,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-using System;
 using MongoDB.Bson;
 using Spark.Engine.Core;
 using Spark.Mongo.Extensions;
@@ -16,24 +15,10 @@ public static class SparkBsonHelper
 {
     // FIXME: Move all extension methods into appropriate classes, i.e. ResourceExtensions, KeyExtensions, etc.
 
-    public static BsonValue ToBsonReferenceKey(this IKey key)
-    {
-        return new BsonString(key.TypeName + "/" + key.ResourceId);
-    }
-
     public static BsonDocument ToBsonDocument(this Entry entry)
     {
         var bsonDocument = entry.Resource.CreateBsonDocument();
         bsonDocument.AddMetaData(entry);
         return bsonDocument;
-    }
-
-    internal static void AssertKeyIsValid(this IKey key)
-    {
-        bool valid = (key.Base == null) && (key.TypeName != null) && (key.ResourceId != null) && (key.VersionId != null);
-        if (!valid)
-        {
-            throw new Exception("This key is not valid for storage: " + key.ToString());
-        }
     }
 }
