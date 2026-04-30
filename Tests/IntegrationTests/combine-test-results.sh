6c2d7dbc363c8aaba9d67cc12b5f4a23566b5b33
Kenneth Myhra <kennethmyhra@gmail.com>
2026-04-30 19:15:33 +0200

    Everywhere: Rename and parameterize existing artifacts

--- Tests/IntegrationTests/combine-test-results.sh
+++ Tests/IntegrationTests/combine-test-results.sh
@@ -4,17 +4,18 @@ set -e
 
 JSON_PATH=$1
 ANNOTATIONS_FILE=$2
+WORKFLOW_FILE=${3:-.github/workflows/integration_tests_r4.yml}
 
 function usage() {
   me=`basename "$0"`
   if [ ! -z "$1" ]; then
     echo $1
   fi
-  echo "Usage: ./${me} path/to/input/json_results path/to/output/annotations.json"
+  echo "Usage: ./${me} path/to/input/json_results path/to/output/annotations.json [workflow_file]"
   exit 1
 }
 
-[ $# -eq 2 ] || usage
+[ $# -ge 2 ] || usage
 
 [ ! -z "${JSON_PATH}" ] || usage
 
@@ -36,11 +37,11 @@ SKIP: \(.skip // 0)"
 
 # Have to use warning annotation level, notice isn't working anymore (but could be in future).
 
-SUMMARY=$(ls _summary*.json | xargs jq '[ .
-  | { "file": ".github/workflow/integration_tests.yml",  "line": 1, "message": ("PASS: \(.pass // 0)\nFAIL: \(.fail // 0)\nERROR: \(.error // 0)\nSKIP: \(.skip // 0)"), "annotation_level": "warning" }
+SUMMARY=$(ls _summary*.json | xargs jq --arg wf "$WORKFLOW_FILE" '[ .
+  | { "file": $wf,  "line": 1, "message": ("PASS: \(.pass // 0)\nFAIL: \(.fail // 0)\nERROR: \(.error // 0)\nSKIP: \(.skip // 0)"), "annotation_level": "warning" }
 ]')
 
-FAILURES=$(ls -I '_summary*.json' | xargs -I '{}' jq '[ .[]
+FAILURES=$(ls -I '_summary*.json' | xargs -I '{}' jq --arg wf "$WORKFLOW_FILE" '[ .[]
     | select(((.status == "skip") and (.message | contains("TODO") | not))
         or .status == "fail"
         or .status == "error")
@@ -50,7 +51,7 @@ FAILURES=$(ls -I '_summary*.json' | xargs -I '{}' jq '[ .[]
     | .description as $description
     | .test_method as $method
     | input_filename as $file
-    | { "file": ".github/workflow/integration_tests.yml",  "line": 1, "message": ($id + ": " + $message), "annotation_level": "failure" }
+    | { "file": $wf,  "line": 1, "message": ($id + ": " + $message), "annotation_level": "failure" }
 ]' '{}')
 
 cd $DIR
