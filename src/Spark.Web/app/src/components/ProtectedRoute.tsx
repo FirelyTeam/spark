d63cda0d90f13666498bca1dff18f9b5d0027195
Kenneth Myhra <kennethmyhra@gmail.com>
2026-07-26 14:00:42 +0200

    deps: Upgraded react-router-dom 7.13.0 -> react-router 8.3.0

--- src/Spark.Web/app/src/components/ProtectedRoute.tsx
+++ src/Spark.Web/app/src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router'
 import { useAuth } from '../hooks/useAuth'
 
 interface ProtectedRouteProps {
--- src/Spark.Web/app/src/main.tsx
+++ src/Spark.Web/app/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './hooks/useAuth'
 import App from './App'
 import './index.css'
