be84a43267c006f073760fca1ee4626ab8111256
Kenneth Myhra <kennethmyhra@gmail.com>
2026-08-14 22:19:53 +0200

    MongoDB: Add TestLogger
    TestLogger is useful for testing purposes where we need to validate that
    log entries has been written.

---
+++ Tests/Spark.Store.MongoDB.Tests/TestLogger.cs
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2026, Incendi <info@incendi.no>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+
+namespace Spark.Store.MongoDB.Tests;
+
+public sealed record LogEntry(LogLevel Level, Exception Exception, string Message);
+
+public sealed class TestLogger<T> : ILogger<T>
+{
+    public ConcurrentQueue<LogEntry> Entries { get; } = new();
+
+    public IDisposable BeginScope<TState>(TState state) where TState : notnull => NullScope.Instance;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(
+        LogLevel logLevel,
+        EventId eventId,
+        TState state,
+        Exception exception,
+        Func<TState, Exception, string> formatter)
+    {
+        Entries.Enqueue(new LogEntry(logLevel, exception, formatter(state, exception)));
+    }
+
+    private sealed class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new();
+
+        public void Dispose()
+        {
+        }
+    }
+}
